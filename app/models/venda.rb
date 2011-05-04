b9553d53308990f670cfe2bc6bb69d9fbdd10b03
Pedro Mamede <pedro.mamede@gmail.com>
2011-05-04 16:43:21 -0300

    Alterações no menu e correção do modelo Venda

--- app/models/venda.rb
+++ app/models/venda.rb
@@ -6,7 +6,7 @@ class Venda < ActiveRecord::Base
 
   validates_presence_of :produto_id, :message => "- Selecione um produto."
   validates_presence_of :vendedor_id, :message => "- Selecione um vendedor."
-  validates_presence_of :produto_id], :message => "- Selecione um cliente."
+  validates_presence_of :produto_id, :message => "- Selecione um cliente."
 
 end
 
