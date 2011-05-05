88a3beeabb8c705a2e75977c6fb6cc8bb82bf1ff
Pedro Mamede <pedro.mamede@gmail.com>
2011-05-05 18:02:06 -0300

    Vendas - CRUD

--- app/controllers/vendas_controller.rb
+++ app/controllers/vendas_controller.rb
@@ -4,5 +4,43 @@ class VendasController < ApplicationController
     @vendas = Venda.order "cliente_id"
   end
 
+  def show
+    @venda = Venda.find(params[:id])
+  end
+
+  def new
+    @venda = Venda.new
+  end
+
+  def create
+    @venda = Venda.new(params[:venda])
+    if @venda.save
+      redirect_to (@venda, :notice => 'Venda efetuada com sucesso!')
+    else
+      render :action => "new"
+    end
+  end
+
+  def update
+    @venda.find(params[:id])
+    puts @venda.id
+    if @venda.update_attributes(params[:venda])
+      redirect_to (@venda, :notice => 'Dados da venda atualizados com sucesso!')
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def edit
+    @venda = Venda.find(params[:id])
+    puts @venda.id
+  end
+
+  def destroy
+    @venda = Venda.find(params[:id])
+    @venda.destroy
+    redirect_to :action => 'index'
+  end
+
 end
 
--- app/models/venda.rb
+++ app/models/venda.rb
@@ -1,12 +1,12 @@
 class Venda < ActiveRecord::Base
 
   belongs_to :produto
-  belongs_to :vendedore
+  belongs_to :vendedor
   belongs_to :cliente
 
   validates_presence_of :produto_id, :message => "- Selecione um produto."
   validates_presence_of :vendedor_id, :message => "- Selecione um vendedor."
-  validates_presence_of :produto_id, :message => "- Selecione um cliente."
+  validates_presence_of :cliente_id, :message => "- Selecione um cliente."
 
 end
 
